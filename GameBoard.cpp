ed7c23cb44d06d688018fb4c71de107622fa7393
Roman M <rmakarov@accusoft.com>
2018-03-25 15:34:07 +0700

    fixed symmetries again

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -25,27 +25,59 @@ GameBoard::GameBoard(SimplyNumbers *simplyGen, Hashtable *movesHash,
 bool GameBoard::put(TMove N) {
                 int x = N%fsize - 7;
                 int y = N/fsize - 7;
+                int x0=x, y0=y;
+                int absX = x < 0 ? -x : x;
+                int absY = y < 0 ? -y : y;
+                bool symmW = history[count-1].symmW;
+                bool swapped = false;
 
-                if (history[count-1].symmX  == 0 && x < 0) {
-                        x = -x;
-                        *swapX = true;
-                }
-                if ((  history[count-1].symmY  == 0 || history[count].symmXY  == 0) && y < 0) {
-                        y = -y;
-                        *swapY = true;
-                }
-                if (  history[count-1].symmW  == 0 && x < y) {
+                if (*swapW) {
                         int t = x;
                         x = y;
                         y = t;
-                        *swapW = true;
+                        swapped = true;
                 }
-                if (  history[count-1].symmXYW  == 0 && x < -y) {
+
+                if (history[count-1].symmX  == 0) {
+                    if (x < 0) {
+                        x = -x;
+                        *swapX = !*swapX;
+                    }
+                }
+                if ((  history[count-1].symmY  == 0 || history[count].symmXY  == 0) && y < 0) {
+                        y = -y;
+                        *swapY = !*swapY;
+                }
+
+                if ( symmW  == 0 && absX < absY) {
                         int t = x;
-                        x = -y;
-                        y = -t;
-                        *swapXYW = true;
+                        x = y;
+                        y = t;
+                        *swapW = !*swapW;
                 }
+
+                if (swapped) {
+                        int t = x;
+                        x = y;
+                        y = t;
+                }
+
+                //*swapX = *swapY = *swapXYW = *swapW = false;
+                        /*
+                if (x!=0 && x==-x0) {
+                  *swapX = !*swapX;
+                }
+                if (y!=0 && y==-y0) {
+                  *swapY = !*swapY;
+                }
+                if (x != y) {
+                  if (x == y0 && y == x0) {
+                    *swapW = !*swapW;
+                  }
+                  if (x == -y0 && y == -x0) {
+                    *swapXYW = !*swapXYW;
+                  }
+                }         */
 
                 return forward((y+7)*15+(x+7));
 };

0d1dff284ed7ee0302ed84250003b8d600abc0a0
Roman M <rmakarov@accusoft.com>
2018-04-25 14:23:17 +0700

    allow to delete only bad moves+

--- hashtable.cpp
+++ hashtable.cpp
@@ -78,7 +78,7 @@ TNode *Hashtable::getOrCreate(THash hX, THash hO, int age, bool &created) {
                 m2 = true;
                 int deviate = medRating - curr->rating;
 
-                if (deviate > maxDeviate && deviate > RATING_OVERWRITE_DELTA){// && curr->age>=11 && curr->age>=age) {
+                if (deviate > maxDeviate && deviate > RATING_OVERWRITE_DELTA && curr->totalChilds == 0){// && curr->age>=11 && curr->age>=age) {
                         maxDeviate = deviate;
                         choosen = curr;
                 }
